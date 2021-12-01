8c4457c932ab7b3edc3d1ae5535aedb7d3d51498
Rafsanjani Aziz <rafsanjani.aziz@sky.uk>
2021-12-01 11:27:52 +0000

    Updates

--- app/src/main/java/com/example/master/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/master/ui/base/BaseActivity.kt
@@ -1,6 +1,9 @@
 package com.example.master.ui.base
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -20,6 +23,13 @@ abstract class BaseActivity<T : ViewDataBinding> : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            statusBarColor = Color.TRANSPARENT
+        }
+
         setupDataBinding()
         setupViewModel()
     }
